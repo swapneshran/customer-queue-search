0dea674d8592e054683776c27d3859c721d5c4c6
swapneshmohanty <swapneshmohanty9220@gmail.com>
2021-05-18 10:47:49 +0100

    removed TODO

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,11 +1,17 @@
 import Customer from "../models/Customer";
 
-const filteredCustomers = (customers: Array<Customer>, customerName: string) => customers.filter(value=> {
-    if(!customerName){
-        return customers
+const filteredCustomers = (customers: Array<Customer> = [], customerName: string) => {
+    console.log(customers)
+    if(!customers){
+        return []
     }
-    return value.customer.name.indexOf(customerName.trim()) !== -1
-})
+    return customers.filter(value=> {
+        if(!customerName){
+            return customers
+        }
+        return value.customer.name.indexOf(customerName.trim()) !== -1
+    })
+}
 
 
 const filterCustomersMatchesWithCustomerName = (customers: Array<Customer>, customerName: string) => {
